82dda54a1a5dfee5b5658138f99465fe8501504b
Jorge A. Benavides <jorge.benavides@wizeline.com>
2022-04-14 16:44:50 -0500

    added compile but crash, and wont compile options

--- ios-meeting-game.playground/Pages/intro.xcplaygroundpage/Contents.swift
+++ ios-meeting-game.playground/Pages/intro.xcplaygroundpage/Contents.swift
@@ -1,5 +1,14 @@
 import UIKit
 
+// Game title: Be the compiler
+
+// Read the code in the snip and determine:
+
+// A) True
+// B) False
+// C) Won't compile
+// D) Compile but crash
+
 // 1
 class Color {
     var alpha: Float = 1
@@ -13,7 +22,7 @@ colorCopy.alpha = 0.5
 print(color.alpha)
 
 // Q view1.alpha will print 0.5?
-// A true
+// A True
 
 //2
 struct Tutorial {
@@ -28,7 +37,7 @@ tutorialCopy.difficulty = 2
 print(tutorial.difficulty)
 
 // Q tutorial.difficulty will print 2?
-// A false
+// A False
 
 // 3
 var thing = "cars"
@@ -42,7 +51,7 @@ thing = "airplanes"
 closure()
 
 // Q What will this code print? "I love airplanes"
-// A false
+// A False
 
 // 4
 let i: Int = 101
@@ -52,7 +61,7 @@ if case 100...101 = i {
 }
 
 // Q will it print?
-// A true
+// A True
 
 // 5
 func greet(_ name: String = "Anonymous") {
@@ -63,19 +72,20 @@ let greetCopy = greet
 greetCopy("Dave")
 
 // Q will it print "Hello, Dave!"?
-// A true
+// A True
 
 // 6
 enum Planet: Int {
-    case Mercury = 1
+    case Mercury
     case Venus
     case Earth
 }
 
-let test = Planet(rawValue: 2)
+let planet: Planet = Planet(rawValue: 3)!
+print(planet)
 
-// Q is the test Type "Plante"?
-// A false
+// Q what is the name of the planet property?
+// A Compile but crash
 
 // 7
 func foo(_ number: Int) -> Int {
@@ -89,14 +99,14 @@ func foo(_ number: Int) -> Int {
 let output = foo(2)
 
 // Q is the ouput 20?
-// A false
+// A False
 
 // 8
 let numbers = [1, 3, 5, 7, 9]
 let result = numbers.reduce(0, +)
 
 // Q is result value = 25?
-// A false
+// A False
 
 // 9
 struct TaylorFan {
@@ -107,4 +117,4 @@ struct TaylorFan {
 let jamesSong = TaylorFan(name: "James").favoriteSong
 
 // Q is jamesSong favorite song "Shake it Off"?
-// A false
+// A Won't Compile
