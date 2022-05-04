f67cf9602dc5c84fde0ffefa88b81c334dcd95e8
Jorge A. Benavides <jorge.benavides@wizeline.com>
2022-05-03 19:53:55 -0500

    intro updated

--- ios-meeting-game.playground/Pages/intro.xcplaygroundpage/Contents.swift
+++ ios-meeting-game.playground/Pages/intro.xcplaygroundpage/Contents.swift
@@ -1,1 +1,34 @@
+"""
 
+# iOS Meeting May 2022
+
+
+## Announcements
+
+Monthly growth - Welcome the new Wizeliners!
+Friendly reminder to finish Annual Security Training
+
+## Releases
+
+### Appstore
+- Upcoming tax and price changes for apps and in-app purchases
+- Small developer assistance submission requests due by May 20.
+    - For more information: https://smallappdeveloperassistance.com/
+
+### Xcode
+- Swift now allows existential types written with the any keyword.
+- Placeholders in type expressions and annotations
+- Improved the accuracy of leak scanning in Instruments, Xcode’s memory graph debugger, and the leaks command line
+
+## Swift tips
+- Follow up with view previews: Playground preview
+
+## Activity
+- Be the compiler! A quizwitz game about swift fundamentals (cuz its fun amirite?)
+
+## Next Host
+- Choose the next host
+
+Link to presentation: https://docs.google.com/presentation/d/1zZ4dqsskLER47IetuVfq_MhxBwDKrI2uIcfs4Dj8ga0
+
+"""
